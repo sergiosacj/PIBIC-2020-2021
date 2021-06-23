f12e321f6be6332fa67953734ee19f025e803fa1
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2021-06-23 10:30:11 -0300

    Add backtrack line search in NewtonCG file

--- algorithms/NewtonCG.jl
+++ algorithms/NewtonCG.jl
@@ -1,6 +1,5 @@
 using NLPModels
 using TimerOutputs
-include("./backtrack_line_search.jl")
 
 # NewtonCG with backtrack line search.
 # outputs
@@ -104,4 +103,30 @@ function conjugategradient(r, B)
 	end
 	
 	return z, jMAX, 1
+end
+
+function backtracklinesearch(x::Array, problemTools, p::Array, ∇f::Array)
+	# output: alpha, iterations, failure
+	alpha = 1
+	c = 1e-4
+	p_lo = 0.1
+	p_hi = 0.5
+	i = 0
+	limit = 1000
+	failure = 0
+
+	∇fTp = sum(∇f.*p)
+	objx = problemTools.obj(x)
+	while problemTools.obj(x + alpha.*p) > objx + c*alpha*∇fTp
+		alpha = min(alpha, alpha*p_hi) # avoid too big reductions
+		alpha = max(alpha, alpha*p_lo) # avoid too small reductions
+
+		i+=1
+		if i>=limit
+			failure = 1
+			break
+		end
+	end
+
+	return alpha, i, failure
 end
