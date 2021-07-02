73e8b262f910b76a7b74c103926484a2bd37829f
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2021-07-02 17:42:31 -0300

    Create script to run tests for ipopt

---
+++ helpers/runIpopt.jl
@@ -0,0 +1,38 @@
+using CUTEst, Printf, TimerOutputs
+using NLPModelsIpopt
+using Suppressor
+include("./print.jl")
+
+function tests(inputFile)
+    io = open(inputFile, "r")
+    # skip first line
+    readline(io)
+    for i = 1:35
+        # reading and parsing
+        name, number, n, m = split(readline(io))
+        number = parse(Int64, number)
+        n = parse(Int64, n)
+        m = parse(Int64, m)
+
+        if name == "ARGAUSS" || name == "ARGLCLE"
+            continue
+        end
+
+        # running
+        println("-----------------------------------------------")
+        println("Started $(name)")
+
+        nlp = CUTEstModel(name)
+        output = @capture_out ipopt(nlp)
+
+        open("Tests/Ipopt/$(name).out", "w") do io
+            write(io, output)
+        end
+
+        finalize(nlp)
+        println("-----------------------------------------------")
+    end
+    close(io)
+end
+
+tests("./helpers/mgh_problems.txt")
