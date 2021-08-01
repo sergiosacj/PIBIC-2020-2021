322702a9d796ac87bd31ce61c63de5117671dbb0
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2021-08-01 18:06:26 -0300

    Update custom runtest script to run specific problems

--- helpers/runtests.jl
+++ helpers/runtests.jl
@@ -1,12 +1,34 @@
 using CUTEst, Printf, TimerOutputs
 include("../algorithms/AR.jl")
 
-function tests()
-    name = "ROSENBR"
+problems =
+[
+"ROSENBR" ,
+"POWELLBS" ,
+"BROWNBS" ,
+"BEALE" ,
+"HELIX" ,
+"BARD" ,
+"BOX3" ,
+"POWELLSG" ,
+"WOODS" ,
+"WATSON" ,
+"SROSENBR" ,
+"VARDIM" ,
+"ARGTRIG" ,
+"BROWNAL" ,
+"BDVALUE" ,
+"INTEGREQ" ,
+"BROYDN3D" ,
+"ARGLINA" ,
+]
+
+function tests(name)
+    solver = "ARp"
     nlp = CUTEstModel(name)
     stop, output = ARp(nlp)
 
-    out = open("Tests/ARp/$(name).out", "w")
+    out = open("Tests/$(solver)/$(name).out", "w")
     printHeader(out)
     printEach(output, out)
     printProblemInfo(output, out, stop)
@@ -15,4 +37,6 @@ function tests()
     finalize(nlp)
 end
 
-tests()
+for problem in problems
+    tests(problem)
+end
